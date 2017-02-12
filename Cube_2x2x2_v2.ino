0ff0543b368219422069e1e8a94282a2264c0ae6
Walookado <walookado@gmail.com>
2017-02-12 01:13:24 -0600

    Tidied Up

--- Cube_2x2x2_v2.ino
+++ Cube_2x2x2_v2.ino
@@ -5,49 +5,49 @@ int layer[2]={A2,A3};
 int led[4]={0,1,4,5};
 int time = 100;
 void setup() {
-  // put your setup code here, to run once:
-   for(int i = 0; i<4; i++)
-  {
-    pinMode(led[i], OUTPUT);
-  }
-  //setting layers to output
-   for(int i = 0; i<2; i++)
-  {
-    pinMode(layer[i], OUTPUT);
-  }
+	// put your setup code here, to run once:
+	for(int i = 0; i<4; i++)
+	{
+		pinMode(led[i], OUTPUT);
+	}
+	//setting layers to output
+	for(int i = 0; i<2; i++)
+	{
+		pinMode(layer[i], OUTPUT);
+	}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Currently set to blink on and off every second.
-  //turnOn();
-  blink();
-
+	// put your main code here, to run repeatedly:
+	//Currently set to blink on and off every second.
+	//turnOn();
+	blink();
+	
 }
 
 void turnOff()
- {
-   for(int i = 0; i<4; i++)
-   {
-     digitalWrite(led[i], 0);
-   }
-   for(int i = 0; i<2; i++)
-   {
-     digitalWrite(layer[i], 1);
-   }
- }
+{
+	for(int i = 0; i<4; i++)
+	{
+		digitalWrite(led[i], 0);
+	}
+	for(int i = 0; i<2; i++)
+	{
+		digitalWrite(layer[i], 1);
+	}
+}
 
 void turnOn()
 {
-  for(int i = 0; i<4; i++)
-  {
-    digitalWrite(led[i], 1);
-  }
-  //turning on layers
-  for(int i = 0; i<2; i++)
-  {
-    digitalWrite(layer[i], 0);
-  }
+	for(int i = 0; i<4; i++)
+	{
+		digitalWrite(led[i], 1);
+	}
+	//turning on layers
+	for(int i = 0; i<2; i++)
+	{
+		digitalWrite(layer[i], 0);
+	}
 }
 
 void blink()
@@ -63,23 +63,22 @@ void blink()
         for(pin = 0; pin < 4; pin++)
         {
             digitalWrite(led[pin], ledVal);
-        }
+		}
         
         for(pin = 0; pin < 2; pin++)
         {
             digitalWrite(layer[pin], layerVal);
-        }
+		}
         if(ledVal == 0)
         {
             ledVal = 1;
             layerVal = 0;
-            
-        }
+		}
         
         else if(ledVal == 1){
             ledVal = 0;
             layerVal = 1;
-        }
+		}
         delay(1000);
-    }
+	}
 }
